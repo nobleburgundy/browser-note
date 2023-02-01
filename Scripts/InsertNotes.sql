93b3ef902908d0954da5ff8cf41716ef5d3317fc
James Gould <nobleburgundy@gmail.com>
2023-02-01 10:17:36 -0600

    Added scripts folder.

---
+++ Scripts/InsertNotes.sql
@@ -0,0 +1,8 @@
+Select Top(100)
+    *
+From Notes;
+
+
+
+-- Insert Into Notes (Title, Text, UserId, CreatedDate, UpdatedDate) 
+-- Values ( 'Test Note 2', 'more testin gtesting', 1, GETDATE(), GETDATE());
