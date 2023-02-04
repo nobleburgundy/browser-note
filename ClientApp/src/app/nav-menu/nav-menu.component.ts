499e6401b04cbb537f83be892761640e5ed4e872
James Gould <nobleburgundy@gmail.com>
2023-02-04 00:53:51 -0600

    Partial functionality to hide nav bar.

--- ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import { faCog } from '@fortawesome/free-solid-svg-icons'
+import { faCog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-menu',
   templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  styleUrls: ['./nav-menu.component.css'],
 })
 export class NavMenuComponent {
   isExpanded = false;
   facog = faCog;
+  showHeader = true;
 
   collapse() {
     this.isExpanded = false;
